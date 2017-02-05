0799462dfeee913de062c61d356d6f968c7921a9
Andy Grunwald <andygrunwald@gmail.com>
2017-02-05 14:27:29 +0100

    Added basic unit test for add command

---
+++ perseus/commands/add_test.go
@@ -0,0 +1,17 @@
+package commands_test
+
+import (
+	"testing"
+	. "github.com/andygrunwald/perseus/perseus/commands"
+)
+
+func TestAddCommand_Run_WithEmptyPackage(t *testing.T) {
+	c := &AddCommand{
+		Package: "",
+	}
+
+	err := c.Run()
+	if err == nil {
+		t.Fatal("Expected error while passing an empty package. Got none")
+	}
+}
