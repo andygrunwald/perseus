792f4475f56e400c3ff25146429810b792515914
Andy Grunwald <andygrunwald@gmail.com>
2017-02-19 20:19:54 +0100

    Fix TODO comment

--- packagist/packagist.go
+++ packagist/packagist.go
@@ -89,7 +89,7 @@ func (c *Client) GetPackage(name string) (*Package, *http.Response, error) {
 	defer resp.Body.Close()
 
 	// Check the status codes
-	// // TODO What happens if Packagist rewrite the package? Which return code we get? 300 something? Or is the redirect handled by the http client? E.g. the facebook example? We should output here both names
+	// TODO What happens if Packagist rewrite the package? Which return code we get? 300 something? Or is the redirect handled by the http client? E.g. the facebook example? We should output here both names
 	if c := resp.StatusCode; c < 200 || c > 299 {
 		return nil, resp, fmt.Errorf("Expected a return code within 2xx for package \"%s\". Got %d", name, c)
 	}
