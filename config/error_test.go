4363ea85451252d1aba0afa5d81c85f5a3c4f85c
Andy Grunwald <andygrunwald@gmail.com>
2017-04-22 20:38:02 +0200

    Add unit test for config/error.go

---
+++ config/error_test.go
@@ -0,0 +1,24 @@
+package config_test
+
+import (
+	"testing"
+	"errors"
+
+	. "github.com/andygrunwald/perseus/config"
+)
+
+func TestIsNoRepositories(t *testing.T) {
+	tests := []struct {
+		err     error
+		result bool
+	}{
+		{ErrNoRepositories, true},
+		{errors.New("Dummy error"), false},
+	}
+
+	for _, tt := range tests {
+		if res := IsNoRepositories(tt.err); res != tt.result {
+			t.Errorf("Expected IsNoRepositories(%+v) to be %+v. Got %+v.", tt.err, tt.result, res)
+		}
+	}
+}
