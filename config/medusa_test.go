b276a7e919ef0296602f65af6e5226abc4c23f5c
Andy Grunwald <andygrunwald@gmail.com>
2017-04-22 23:09:04 +0200

    Added a few more unit tests for Medusa

--- config/medusa_test.go
+++ config/medusa_test.go
@@ -94,26 +94,33 @@ func TestMedusa_GetRepositoryURLOfPackage_CorrectRepositories(t *testing.T) {
 	}
 }
 
-func TestMedusa_GetNamesOfRepositories_FaultyRepositories(t *testing.T) {
+func TestMedusa_GetNamesOfRepositories(t *testing.T) {
+	provider := &MedusaUnitTestProvider{}
+	numOfPackages := 2
+
+	m, err := NewMedusa(provider)
+	if err != nil {
+		t.Errorf("NewMedusa(Provider) throws error: %s", err)
+	}
+
+	l, err := m.GetNamesOfRepositories()
+	if err != nil {
+		t.Errorf("Expected no error for provider %T. Got %s", provider, err)
+	}
+	if n := len(l); n != numOfPackages {
+		t.Errorf("Expected a different number of packages. Got %d, expected %d for provider %T", n, numOfPackages, provider)
+	}
+}
+
+func TestMedusa_GetNamesOfRepositories_EmptyRepositoryList(t *testing.T) {
 	tests := []struct {
-		name     string
 		provider Provider
 		num      int
 	}{
 		// No "repositories" configured at all
-		{"symfony/console", &EmptyUnitTestProvider{}, 0},
+		{&EmptyUnitTestProvider{}, 0},
 		// "repositories" key is there, but empty
-		{"key/exists", &EmptyWithKeysUnitTestProvider{}, 0},
-		/*
-			// Repositories configured, but not this one
-			{"twig/twig", &MedusaUnitTestProvider{}, 0},
-			// Repositories configured, plus this one, but doesn't have an url key
-			{"no/url", &MedusaUnitTestProvider{}, 0},
-			// Repositories configured, plus this one, but has an empty url key
-			{"empty/url", &MedusaUnitTestProvider{}, 0},
-			// Repositories configured, plus this one, but has an invalid url key
-			{"invalid/url", &MedusaUnitTestProvider{}, 0},
-		*/
+		{&EmptyWithKeysUnitTestProvider{}, 0},
 	}
 
 	for _, tt := range tests {
@@ -123,11 +130,50 @@ func TestMedusa_GetNamesOfRepositories_FaultyRepositories(t *testing.T) {
 		}
 
 		l, err := m.GetNamesOfRepositories()
-		if l != nil {
-			t.Errorf("Package '%s': Expected url to be nil. Got: %+v", tt.name, l)
+		if n := len(l); n != tt.num {
+			t.Errorf("Expected a different number of packages. Got %d, expected %d for provider %T", n, tt.num, tt.provider)
 		}
 		if err == nil {
-			t.Errorf("Package '%s': No error thrown.", tt.name)
+			t.Errorf("No error thrown. Expected an error for provider %T", tt.provider)
 		}
+	}
+}
+
+func TestMedusa_GetRequire(t *testing.T) {
+	tests := []struct {
+		provider Provider
+		num      int
+	}{
+		// Empty "require" configured
+		{&EmptyUnitTestProvider{}, 0},
+		// 3 "require"s configured
+		{&MedusaUnitTestProvider{}, 3},
+	}
+
+	for _, tt := range tests {
+		m, err := NewMedusa(tt.provider)
+		if err != nil {
+			t.Errorf("NewMedusa(Provider) throws error: %s", err)
+		}
+
+		l := m.GetRequire()
+		if n := len(l); n != tt.num {
+			t.Errorf("Expected a different number of packages. Got %d, expected %d for provider %T", n, tt.num, tt.provider)
+		}
+	}
+}
+
+func TestMedusa_GetString(t *testing.T) {
+	provider := &MedusaUnitTestProvider{}
+	key := "repodir"
+
+	m, err := NewMedusa(provider)
+	if err != nil {
+		t.Errorf("NewMedusa(Provider) throws error: %s", err)
+	}
+
+	s := m.GetString(key)
+	if len(s) == 0 {
+		t.Errorf("Expected a non empty string. Got an empty string for key %s", key)
 	}
 }
